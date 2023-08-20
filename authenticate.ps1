9ce37e39bd25e3e9e331312a06fd4d3e271bc680
KarthikMurugesan98 <140873882+KarthikMurugesan98@users.noreply.github.com>
2023-08-20 13:55:45 +0530

    Update authenticate.ps1

--- authenticate.ps1
+++ authenticate.ps1
@@ -3,13 +3,15 @@ $authHeaders = @{
 }
 
 $authBody = @{
-   'grant_type'    = 'refresh_token'
-   'client_id'     = $env:ORCHESTRATOR_APP_ID
-   'refresh_token' = $env:ORCHESTRATOR_APP_SECRET
+   grant_type    = 'refresh_token'
+   client_id     = $env:ORCHESTRATOR_APP_ID
+   refresh_token = $env:ORCHESTRATOR_APP_SECRET
 }
 
+$encodedAuthBody = $authBody | ForEach-Object { "$($_.Key)=$($_.Value)" } -join '&'
+
 try {
-   $authResponse = Invoke-RestMethod -Uri "https://cloud.uipath.com/karthiktecnology/DefaultTenant/identity/connect/token" -Headers $authHeaders -Method Post -Body ($authBody | ConvertTo-Json)
+   $authResponse = Invoke-RestMethod -Uri "https://cloud.uipath.com/karthiktecnology/DefaultTenant/identity/connect/token" -Headers $authHeaders -Method Post -Body $encodedAuthBody
    if ($authResponse -and $authResponse.access_token) {
       Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
    } else {
