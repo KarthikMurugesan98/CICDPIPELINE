6a395a357a30477f0ccb98a09993bf04c4c86527
KarthikMurugesan98 <140873882+KarthikMurugesan98@users.noreply.github.com>
2023-08-20 13:47:45 +0530

    Update authenticate.ps1

--- authenticate.ps1
+++ authenticate.ps1
@@ -1,12 +1,20 @@
 $authHeaders = @{
-   'Content-Type' = 'application/json'
+   'Content-Type' = 'application/x-www-form-urlencoded'
 }
 
 $authBody = @{
-   "grant_type"    = "refresh_token"
-   "client_id"     = $env:ORCHESTRATOR_APP_ID
-   "refresh_token" = $env:ORCHESTRATOR_APP_SECRET
+   'grant_type'    = 'refresh_token'
+   'client_id'     = $env:ORCHESTRATOR_APP_ID
+   'refresh_token' = $env:ORCHESTRATOR_APP_SECRET
 }
 
-$authResponse = Invoke-RestMethod -Uri "$env:ORCHESTRATOR_URL/identity/connect/token" -Headers $authHeaders -Method Post -Body ($authBody | ConvertTo-Json)
-Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
+try {
+   $authResponse = Invoke-RestMethod -Uri "https://cloud.uipath.com/karthiktecnology/DefaultTenant/identity/connect/token" -Headers $authHeaders -Method Post -Body ($authBody | ConvertTo-Json)
+   if ($authResponse -and $authResponse.access_token) {
+      Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
+   } else {
+      throw "Authentication failed. Check your credentials and endpoint URL."
+   }
+} catch {
+   throw "An error occurred during authentication: $_"
+}
