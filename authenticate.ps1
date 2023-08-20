645cf2f532ced139d8dbd3ed4cdfeb4ea1fd304a
KarthikMurugesan98 <140873882+KarthikMurugesan98@users.noreply.github.com>
2023-08-20 13:42:39 +0530

    Update authenticate.ps1

--- authenticate.ps1
+++ authenticate.ps1
@@ -1,20 +1,12 @@
 $authHeaders = @{
-   'Content-Type' = 'application/x-www-form-urlencoded'
+   'Content-Type' = 'application/json'
 }
 
 $authBody = @{
-   'grant_type'    = 'refresh_token'
-   'client_id'     = $env:ORCHESTRATOR_APP_ID
-   'refresh_token' = $env:ORCHESTRATOR_APP_SECRET
+   "grant_type"    = "refresh_token"
+   "client_id"     = $env:ORCHESTRATOR_APP_ID
+   "refresh_token" = $env:ORCHESTRATOR_APP_SECRET
 }
 
-try {
-   $authResponse = Invoke-RestMethod -Uri "https://cloud.uipath.com/your-tenant-name/DefaultTenant/identity/connect/token" -Headers $authHeaders -Method Post -Body ($authBody | ConvertTo-Json)
-   if ($authResponse -and $authResponse.access_token) {
-      Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
-   } else {
-      throw "Authentication failed. Check your credentials and endpoint URL."
-   }
-} catch {
-   throw "An error occurred during authentication: $_"
-}
+$authResponse = Invoke-RestMethod -Uri "$env:ORCHESTRATOR_URL/identity/connect/token" -Headers $authHeaders -Method Post -Body ($authBody | ConvertTo-Json)
+Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
