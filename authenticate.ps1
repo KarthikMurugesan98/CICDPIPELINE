866e9560844021b7c32b3b39aed9b28f1101c442
KarthikMurugesan98 <140873882+KarthikMurugesan98@users.noreply.github.com>
2023-08-20 14:03:17 +0530

    Update authenticate.ps1

--- authenticate.ps1
+++ authenticate.ps1
@@ -1,22 +1,19 @@
 $authHeaders = @{
-   'Content-Type' = 'application/x-www-form-urlencoded'
+    'Content-Type' = 'application/x-www-form-urlencoded'
 }
 
 $authBody = @{
-   grant_type    = 'refresh_token'
-   client_id     = $env:ORCHESTRATOR_APP_ID
-   refresh_token = $env:ORCHESTRATOR_APP_SECRET
+    'grant_type'    = 'refresh_token';
+    'client_id'     = $env:ORCHESTRATOR_APP_ID;
+    'refresh_token' = $env:ORCHESTRATOR_APP_SECRET;
 }
 
-$encodedAuthBody = $authBody | ForEach-Object { "$($_.Key)=$($_.Value)" } -join '&'
+$encodedBody = $authBody | ForEach-Object { "$($_.Key)=$($_.Value)" } -join '&'
 
-try {
-   $authResponse = Invoke-RestMethod -Uri "https://cloud.uipath.com/karthiktecnology/DefaultTenant/identity/connect/token" -Headers $authHeaders -Method Post -Body $encodedAuthBody
-   if ($authResponse -and $authResponse.access_token) {
-      Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
-   } else {
-      throw "Authentication failed. Check your credentials and endpoint URL."
-   }
-} catch {
-   throw "An error occurred during authentication: $_"
+$authResponse = Invoke-RestMethod -Uri "$env:ORCHESTRATOR_URL/identity/connect/token" -Method Post -Headers $authHeaders -Body $encodedBody
+
+if ($authResponse.access_token) {
+    Write-Output "::set-output name=accessToken::$($authResponse.access_token)"
+} else {
+    throw "Authentication failed. Check your credentials and endpoint URL."
 }
